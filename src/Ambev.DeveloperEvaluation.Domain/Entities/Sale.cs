f6f0f0d113a1435789d167c9349b794361f218d4
René Bizelli <renebizelli@gmail.com>
2025-07-27 16:23:18 -0300

    Add sales mapping

--- src/Ambev.DeveloperEvaluation.Domain/Entities/Sale.cs
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/Sale.cs
@@ -12,6 +12,22 @@ public class Sale
     public DateTime CreatedAt { get; set; }
     public ICollection<SaleItem> Items { get; set; } = [];
 
+    public class SaleItem
+    {
+        public SaleProduct Product { get; set; } = new();
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        public SaleItemStatus Status { get; set; }
+        public decimal TotalPrice { get { return (Price * Quantity) - Discount; } }
+    }
+
+    public class SaleProduct
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+    }
+
     public class SaleBranch
     {
         public Guid Id { get; set; }
@@ -22,5 +38,7 @@ public class Sale
     {
         public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
     }
 }
---
+++ src/Ambev.DeveloperEvaluation.NoSQL.MongoDB/Mapping/SaleMapping.cs
@@ -0,0 +1,51 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using MongoDB.Bson.Serialization;
+using static Ambev.DeveloperEvaluation.Domain.Entities.Sale;
+
+namespace Ambev.DeveloperEvaluation.NoSQL.MongoDB.Mapping;
+
+public class SaleMapping
+{
+    public SaleMapping()
+    {
+        BsonClassMap.RegisterClassMap<Sale>(map =>
+        {
+            map.MapIdProperty(p => p.Id);
+            map.MapProperty(p => p.Branch).SetElementName("branch");
+            map.MapProperty(p => p.User).SetElementName("user");
+            map.MapProperty(p => p.TotalAmount).SetElementName("total");
+            map.MapProperty(p => p.CreatedAt).SetElementName("create");
+            map.MapProperty(p => p.Items).SetElementName("items");
+        });
+
+        BsonClassMap.RegisterClassMap<SaleBranch>(map =>
+        {
+            map.MapProperty(p => p.Id).SetElementName("id");
+            map.MapProperty(p => p.Name).SetElementName("name");
+        });
+
+        BsonClassMap.RegisterClassMap<SaleUser>(map =>
+        {
+            map.MapProperty(p => p.Id).SetElementName("id");
+            map.MapProperty(p => p.Name).SetElementName("name");
+            map.MapProperty(p => p.City).SetElementName("city");
+            map.MapProperty(p => p.State).SetElementName("state");
+        });
+
+        BsonClassMap.RegisterClassMap<SaleItem>(map =>
+        {
+            map.MapProperty(p => p.Product).SetElementName("product");
+            map.MapProperty(p => p.Quantity).SetElementName("quantity");
+            map.MapProperty(p => p.Price).SetElementName("price");
+            map.MapProperty(p => p.Discount).SetElementName("discount");
+            map.MapProperty(p => p.Status).SetElementName("Status");
+            map.MapProperty(p => p.TotalPrice).SetElementName("total");
+        });
+
+        BsonClassMap.RegisterClassMap<SaleProduct>(map =>
+        {
+            map.MapProperty(p => p.Id).SetElementName("id");
+            map.MapProperty(p => p.Name).SetElementName("name");
+        });
+    }
+}
