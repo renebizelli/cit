41126c4540231e0fa57aa49e7b84aedf1dc53cb1
René Bizelli <renebizelli@gmail.com>
2025-07-27 16:13:01 -0300

    Add product entities

---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/Sale.cs
@@ -0,0 +1,26 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Enums;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities;
+
+public class Sale
+{
+    public string Id { get; set; } = string.Empty;
+    public SaleBranch Branch { get; set; } = new();
+    public SaleStatus Status { get; set; }
+    public SaleUser User { get; set; } = new();
+    public decimal TotalAmount { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public ICollection<SaleItem> Items { get; set; } = [];
+
+    public class SaleBranch
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+
+    public class SaleUser
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
